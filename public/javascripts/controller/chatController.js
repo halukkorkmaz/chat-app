0fe34463132e62beb2003f3e360c5ee90874564b
Abdulhalik <abdlhlkkrkmz@gmail.com>
2019-12-10 00:52:56 +0300

    fixed tabs in chat route

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -1,3 +1,8 @@
 app.controller('chatController', ['$scope', ($scope) => {
+    $scope.activeTab = 2;
+
+    $scope.changeTab = tab => {
+        $scope.activeTab = tab; // Chats Tab = 1, Online Users Tab = 2
+    }
     const socket = io.connect("http://localhost:3000");
 }]);
