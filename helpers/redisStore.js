cc7438eff0c0211c16ec3b1aec548fb0bc613693
Abdulhalik <abdlhlkkrkmz@gmail.com>
2019-12-09 23:36:23 +0300

    recommited added socket.io-redis and passport.socketio

--- helpers/redisStore.js
+++ helpers/redisStore.js
@@ -1,7 +1,11 @@
 const session = require('express-session');
+const redis = require('redis');
 const redisStore = require('connect-redis')(session);
 
+const redisClient = redis.createClient();
+
 module.exports = new redisStore({
+    client: redisClient,
     host: process.env.REDIS_URI,
     port: process.env.REDIS_PORT,
     pass: process.env.REDIS_PASS
--- src/socketApi.js
+++ src/socketApi.js
@@ -1,13 +1,35 @@
 const socketio = require('socket.io');
-
+const socketAuthorization = require('../middleware/socketAuthorization');
 const io = socketio();
 
 const socketApi = {
     io: io
 };
 
+// Socket authorization
+io.use(socketAuthorization);
+
+/**
+ * Redis adapter
+ * Amaç birden fazla server kullanıldığı durumda
+ * bir server lokalinde kalmayıp diper sunuculara da
+ * yayının yapılması sağlanır.
+ * 3000 portu ve 3002 port örneği.
+ * socket bağlantılarını tek bir redis server
+ * üzerinden senkronize etmek.
+ */
+
+
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({
+    host: process.env.REDIS_URI,
+    port: process.env.REDIS_PORT
+}));
+
 io.on('connection', socket => {
-    console.log('a user logged in');
+    console.log('a user logged in with name ' + socket.request.user.name);
+
+    socket.broadcast.emit('hello');
 });
 
 module.exports = socketApi;
